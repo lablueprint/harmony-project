472ca15f3409422eb0f07718b7661e8e541ff214
leo-liu1 <leoliu1@g.ucla.edu>
2020-02-12 20:10:38 -0800

    remade repo, init commit

---
+++ src/Home/HomeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
