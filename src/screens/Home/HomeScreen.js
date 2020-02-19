270571cf5c67c70a3a01b6f9a65194288c341aa6
rtphan <rtphan@g.ucla.edu>
2020-02-18 19:43:51 -0800

    Rebase Code + Initialized Auth Structure

---
+++ src/screens/Home/HomeScreen.js
@@ -0,0 +1,26 @@
+// import React, { useState, useEffect } from 'react';
+// import { Button, View, Text } from 'react-native';
+
+// export default function Home({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home It's updated</Text>
+//     </View>
+//   );
+// }
+
+// Home.navigationOptions = ({ navigation }) => ({
+//   title: 'Home',
+// });
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
