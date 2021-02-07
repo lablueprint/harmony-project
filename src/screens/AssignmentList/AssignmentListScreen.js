23790f4e8177b9e152603f31f2fb634e78a9657e
Kendrake Tsui <kjtsui@yahoo.com>
2021-02-06 22:12:57 -0800

    Integrated upload file button for each student assignment

--- src/screens/AssignmentList/AssignmentListScreen.js
+++ src/screens/AssignmentList/AssignmentListScreen.js
@@ -4,10 +4,9 @@ import {
   Text, View, ScrollView, StyleSheet, Button,
 } from 'react-native';
 import Firestore from '@react-native-firebase/firestore';
+import firebase from '@react-native-firebase/app';
 import PropTypes from 'prop-types';
 import Post from '../../components/Post/Post';
-import UploadFile from '../../components/UploadFile/UploadFile';
-// import UploadFile from '../../components/UploadFile';
 
 const styles = StyleSheet.create({
 
@@ -37,65 +36,112 @@ const styles = StyleSheet.create({
 export default function AssignmentListScreen({ navigation }) {
   const [errorMessage, setErrorMessage] = useState(null);
   const [postsList, setPostsList] = useState([]);
+  const uid = navigation.getParam('uid', null);
+
+  const createSubmission = ((userID, postID, body, attachment) => {
+    // TODO: see if doc already exists. If it does, update the body, attachment, and updatedAt
+    console.log(userID);
+    Firestore().collection('submissions').where('authorID', '==', userID).where('assignmentID', '==', postID)
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size > 0) { // update if submission exists
+          // querySnapshot.forEach((doc) => console.log(doc));
+          const docID = querySnapshot.docs[0].id;
+          Firestore().collection('submissions').doc(docID).update({
+            updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+            body,
+            attachment,
+          })
+            .then(() => {
+              console.log('Successfully updated document!');
+            })
+            .catch((error) => {
+              console.log('Submission update failed');
+              console.log(error);
+            });
+        } else { // do this to add submission if it doesn't exist
+          Firestore().collection('submissions').add({
+            authorID: userID,
+            assignmentID: postID,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+            body,
+            attachment,
+            hasReceivedFeedback: false,
+          }).then(() => {
+            console.log('Successfully inserted document!');
+          })
+            .catch((error) => {
+              console.log('Submission failed ):');
+              console.log(error);
+            });
+        }
+      });
+  });
 
   useEffect(() => {
-    Firestore().collection('assignments')
-      .get()
+    // Gets all the classrooms
+    Firestore().collection('classrooms').get()
       .then((snapshot) => {
-        const posts = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-        setPostsList(posts.map((post) => {
-          const date = post.createdAt.toDate();
-          if (!post.hasBeenSubmitted) {
-            return (
-              <View style={styles.container} key={post.id}>
-                <Post
-                  key={post.id}
-                  name={post.username}
-                  title={post.title}
-                  createdAt={date.toTimeString()}
-                  date={date.toDateString()}
-                  attachment={post.attachment}
-                  body={post.body}
-                >
-                  {post.body}
-                </Post>
-                <Button
-                  styles={styles.container}
-                  title="Comment on Post"
-                  onPress={() => {
-                    navigation.navigate('NewComment', { ID: post.id });
-                  }}
-                />
-                <UploadFile
-                  docId={post.id}
-                  collection="recordings"
-                />
-              </View>
-            );
+        const studentClassroomIDs = []; // array of all classrooms a student is in
+        const classrooms = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        for (let i = 0; i < classrooms.length; i += 1) {
+          if (classrooms[i].studentIDs.includes(uid)) {
+            studentClassroomIDs.push(classrooms[i].id);
           }
-          return (
-            <View style={styles.container} key={post.id}>
-              <Post
-                key={post.id}
-                name={post.username}
-                title={post.title}
-                createdAt={date.toTimeString()}
-                date={date.toDateString()}
-                attachment={post.attachment}
-                body={post.body}
-              >
-                {post.body}
-              </Post>
-              <Button
-                styles={styles.container}
-                title="Comment on Post"
-                onPress={() => {
-                  navigation.navigate('NewComment', { ID: post.id });
-                }}
-              />
-            </View>
-          );
-        }));
+        }
+        return studentClassroomIDs;
+      })
+      .then((studentClassroomIDs) => {
+        // Gets all the assignments
+        Firestore().collection('assignments').limit(6)
+          .orderBy('createdAt', 'desc')
+          .get()
+          .then((snapshot) => {
+            // This creates an object for each document
+            const posts = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setPostsList(posts.map((post) => {
+              const date = post.createdAt.toDate();
+              // Only returns assignments with classroomIDs in classroomIDsList
+              if (studentClassroomIDs.includes(post.classroomID)) {
+                return (
+                  <View style={styles.container} key={post.id}>
+                    <Post
+                      key={post.id}
+                      name={post.username}
+                      title={post.title}
+                      createdAt={date.toTimeString()}
+                      date={date.toDateString()}
+                      attachment={post.attachment}
+                      body={post.body}
+                    >
+                      {post.body}
+                    </Post>
+                    <Button
+                      styles={styles.container}
+                      title="Comment on Post"
+                      onPress={() => {
+                        navigation.navigate('NewComment', { ID: post.id });
+                      }}
+                    />
+                    <Button
+                      title="Upload Submission"
+                      onPress={() => {
+                        navigation.navigate('NewPost', {
+                          userID: uid,
+                          postID: post.id,
+                          collection: 'recordings',
+                          title: post.title,
+                          handleSubmit: createSubmission,
+                        });
+                      }}
+                    />
+                  </View>
+                );
+              }
+              return null;
+            }));
+          });
       })
       .catch((error) => {
         setErrorMessage(error.message);
@@ -121,9 +167,7 @@ export default function AssignmentListScreen({ navigation }) {
             navigation.navigate('Post');
           }}
         />
-
       </ScrollView>
-
     </View>
   );
 }
