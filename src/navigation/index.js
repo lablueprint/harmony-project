f415c5529f23378af6692f3e9d5bfcdeddb847c4
Cody Do <D.Codyx@gmail.com>
2021-04-28 21:00:20 -0700

    Added Auth

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Firebase from '@react-native-firebase/app';
 import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigation from './AuthNavigation';
 import BottomTabNavigator from './BottomTabNavigation';
 
-const AppContainer = () => (
-  <NavigationContainer>
-    <BottomTabNavigator />
-  </NavigationContainer>
-);
+const AppContainer = () => {
+  const [isAuth, setAuth] = useState(null);
+
+  // Check if user is logged in. If yes then isAuth is != null
+  useEffect(() => {
+    setAuth(Firebase.auth().currentUser);
+  }, []);
+
+  return (
+    <NavigationContainer>
+      { isAuth && <BottomTabNavigator /> }
+      { !isAuth && <AuthNavigation /> }
+    </NavigationContainer>
+  );
+};
 
 export default AppContainer;
